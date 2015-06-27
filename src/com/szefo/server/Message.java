b93c4082ccea4ef2b8297054f3b6c76274f64a56
szefo <nhat997@gmail.com>
2015-06-27 10:54:14 +0200

    message

--- src/com/szefo/server/Message.java
+++ src/com/szefo/server/Message.java
@@ -1,5 +1,59 @@
 package com.szefo.server;
 
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable{
+
+    private String sender, recipient, content, type;
+
+
+    public Message(String sender, String recipient, String content, String type) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "sender='" + sender + '\'' +
+                ", recipient='" + recipient + '\'' +
+                ", content='" + content + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
